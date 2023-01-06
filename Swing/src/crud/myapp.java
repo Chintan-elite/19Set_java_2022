0d42aaeada2bb8c3c62faa8f4ccec12b913567f7
Chintan <chintan.tops@gmail.com>
2023-01-06 15:05:38 +0530

    js

--- Swing/src/crud/myapp.java
+++ Swing/src/crud/myapp.java
@@ -10,13 +10,19 @@ import java.awt.BorderLayout;
 import java.awt.Font;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+
+import net.proteanit.sql.DbUtils;
+
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.awt.event.ActionEvent;
+import javax.swing.JTable;
+import javax.swing.JScrollPane;
 
 public class myapp {
 
@@ -47,6 +53,7 @@ public class myapp {
 	public myapp() {
 		initialize();
 		connect();
+		loadData();
 	}
 
 	/**
@@ -54,15 +61,32 @@ public class myapp {
 	 */
 	
 	static Connection cn = null;
+	private JTable table;
+	private JTextField sid;
 	public static void connect()
 	{
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
-			cn = DriverManager.getConnection("jdbc:mysql://localhost:3306/practice?user=root&&password=root");
+			cn = DriverManager.getConnection("jdbc:mysql://localhost:3306/practice?user=root");
 		} catch (ClassNotFoundException | SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	}
+	
+	public void loadData()
+	{
+		PreparedStatement ps;
+		try {
+			ps = cn.prepareStatement("select * from std");
+			ResultSet rs = ps.executeQuery();
+			table.setModel(DbUtils.resultSetToTableModel(rs));
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
 	
 	private void initialize() {
@@ -129,6 +153,7 @@ public class myapp {
 						fname1.setText("");
 						lname1.setText("");
 						email1.setText("");
+						loadData();
 					}
 				
 				} catch (SQLException e1) {
@@ -139,5 +164,130 @@ public class myapp {
 		});
 		btnNewButton.setBounds(114, 129, 115, 23);
 		panel.add(btnNewButton);
+		
+		JButton btnNewButton_2 = new JButton("Update");
+		btnNewButton_2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			
+				int id = Integer.parseInt(sid.getText());
+				String stfname = fname1.getText();
+				String stlname = lname1.getText();
+				String stemail = email1.getText();
+				
+				try {
+					PreparedStatement ps = cn.prepareStatement("update std set fname=?,lname=?,email=? where id=?");
+					ps.setInt(4, id);
+					ps.setString(1, stfname);
+					ps.setString(2, stlname);
+					ps.setString(3, stemail);
+					int i = ps.executeUpdate();
+					if(i>0)
+					{
+						JOptionPane.showMessageDialog(frame,"Data updated !!!");
+						fname1.setText("");
+						lname1.setText("");
+						email1.setText("");
+						loadData();
+					}
+				
+				} catch (SQLException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			
+			
+			}
+		});
+		btnNewButton_2.setBounds(114, 163, 115, 23);
+		panel.add(btnNewButton_2);
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBounds(0, 0, 10, 10);
+		frame.getContentPane().add(panel_1);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(309, 58, 284, 199);
+		frame.getContentPane().add(scrollPane);
+		
+		JPanel panel_2 = new JPanel();
+		scrollPane.setViewportView(panel_2);
+		
+		table = new JTable();
+		panel_2.add(table);
+		
+		JPanel panel_3 = new JPanel();
+		panel_3.setBounds(32, 268, 267, 80);
+		frame.getContentPane().add(panel_3);
+		panel_3.setLayout(null);
+		
+		JLabel lblNewLabel_1 = new JLabel("ID");
+		lblNewLabel_1.setBounds(28, 11, 56, 14);
+		panel_3.add(lblNewLabel_1);
+		
+		sid = new JTextField();
+		sid.setBounds(61, 8, 86, 20);
+		panel_3.add(sid);
+		sid.setColumns(10);
+		
+		JButton btnNewButton_1 = new JButton("Delete");
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				int id = Integer.parseInt(sid.getText());
+				PreparedStatement ps;
+				try {
+					ps = cn.prepareStatement("delete from std where id=?");
+				
+					ps.setInt(1, id);
+					int i=ps.executeUpdate();
+					if(i>0) {
+						JOptionPane.showMessageDialog(frame, "data deleted !!!");
+						loadData();
+					}
+					else
+					{
+						JOptionPane.showMessageDialog(frame, "data not found !!!");
+						loadData();
+					}
+				} catch (SQLException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			
+			}
+		});
+		btnNewButton_1.setBounds(168, 7, 89, 23);
+		panel_3.add(btnNewButton_1);
+		
+		JButton btnNewButton_1_1 = new JButton("Edit");
+		btnNewButton_1_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			
+				int id = Integer.parseInt(sid.getText());
+				try {
+					PreparedStatement ps = cn.prepareStatement("select * from std where id = ?");
+					ps.setInt(1, id);
+					ResultSet rs = ps.executeQuery();
+					if(rs.next())
+					{
+						fname1.setText(rs.getString(2));
+						lname1.setText(rs.getString(3));
+						email1.setText(rs.getString(4));
+					}
+					else
+					{
+						JOptionPane.showMessageDialog(frame, "data not found !!!");
+						loadData();
+					}
+					
+				} catch (SQLException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			
+			}
+		});
+		btnNewButton_1_1.setBounds(168, 41, 89, 23);
+		panel_3.add(btnNewButton_1_1);
 	}
 }
